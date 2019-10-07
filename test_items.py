f4c2f8fec37ad12834de9aaddb824d4b8c17380d
Michael-gitbash <michael.co@ya.ru>
2019-10-07 17:07:25 +0300

    Rename test

--- test_items.py
+++ test_items.py
@@ -7,7 +7,7 @@ from selenium.webdriver.common.by import By
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_guest_should_see_login_link(browser: webdriver.Chrome):
+def test_check_add_to_basket_button(browser: webdriver.Chrome):
     print("start test")
     browser.get(link)
     # time.sleep(30)
